0919f42d56b32036ec7a1b60e8fd8eb130b30adb
Alexander Dahmen <dahmen@werdenktwas.de>
2025-12-02 14:51:24 +0100

    Added custom IQB SVG icon sheet, WIP custom icons

--- projects/molecule-editor-widget/src/components/molecule-editor/molecule-editor.component.ts
+++ projects/molecule-editor-widget/src/components/molecule-editor/molecule-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { MatIcon, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MatButton } from '@angular/material/button';
 import { MatDrawer, MatDrawerContainer } from '@angular/material/sidenav';
 import { PsService, PsTable, PsTableInteractionsDirective } from 'periodic-system-common';
@@ -7,6 +8,7 @@ import { MoleculeEditorService } from '../../services/molecule-editor.service';
 import { MoleculeEditorPickerService } from '../../services/molecule-editor-picker.service';
 import { EditorCanvas } from '../editor-canvas/editor-canvas';
 import { EditorControls } from '../editor-controls/editor-controls';
+import IqbIcons from '../../assets/iqb-icons.svg';
 
 @Component({
   selector: 'app-molecule-editor',
@@ -30,4 +32,16 @@ import { EditorControls } from '../editor-controls/editor-controls';
 })
 export class MoleculeEditor {
   readonly service = inject(MoleculeEditorService);
+
+  protected readonly iconRegistry = inject(MatIconRegistry);
+  protected readonly domSanitizer = inject(DomSanitizer);
+
+  constructor() {
+    this.registerCustomSvgIcons();
+  }
+
+  private registerCustomSvgIcons() {
+    const safeIqbIcons = this.domSanitizer.bypassSecurityTrustHtml(IqbIcons);
+    this.iconRegistry.addSvgIconSetLiteralInNamespace('iqb', safeIqbIcons);
+  }
 }
